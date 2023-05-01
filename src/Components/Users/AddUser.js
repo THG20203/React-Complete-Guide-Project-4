c48d5a4769f6d40396ebcaf52da1676a49be4a5c
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-01 20:57:39 +0100

    moving back to simple div wrapper but with array notes

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -48,16 +48,22 @@ const AddUser = (props) => {
     thing. Even array -> just one object so you still only return one array, one object so
     not two arrays at the same time. Wrapping [] is a work around -any element would work
     including custom components -> only thing that matters = one value -> return.  */
+
     /* we've put dynamic expression as first value in the array with a comma, curly braces 
     had to be removed for the first expression because we're no longer inside JSX. */
-    [
-      error && (
+
+    /* IMPORTANT NOTES -> We can retrun an array, because React is able to work with arrays 
+    of JSX elements. i.e. mapping arrays of data to JSX elements. BUT will get warning as is, 
+    because whenever you're working with an array of JSX elements, React wants a key on every 
+    element. so easier to just use <div> to wrap. */
+    <div>
+      {error && (
         <ErrorModal
           title={error.title}
           message={error.message}
           onConfirm={errorHandler}
         />
-      ),
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
@@ -76,8 +82,8 @@ const AddUser = (props) => {
           />
           <Button type="submit">Add User</Button>
         </form>
-      </Card>,
-    ]
+      </Card>
+    </div>
   );
 };
 
