ffc47a19144e5db482276b4c249b1eee21c9c6fc
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-01 22:43:11 +0100

    created second modaloverlay portal

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -24,22 +24,22 @@ const Backdrop = (props) => {
   <div className={classes.backdrop} onClick={props.onConfirm} />;
 };
 
+const ModalOverlay = (props) => {
+  <Card className={classes.modal}>
+    <header className={classes.header}>
+      <h2>{props.title}</h2>
+    </header>
+    <div className={classes.content}>
+      <p>{props.message}</p>
+    </div>
+    <footer className={classes.actions}>
+      <Button onClick={props.onConfirm}>Okay</Button>
+    </footer>
+  </Card>;
+};
+
 const ErrorModal = (props) => {
-  return (
-    <React.fragment>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </React.fragment>
-  );
+  return <React.fragment>{/* In the fragment */}</React.fragment>;
 };
 
 export default ErrorModal;
